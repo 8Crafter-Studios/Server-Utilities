50ab12239545f6de10de6bc09735387d8b3334fe
Andexter8 <8crafteryt@gmail.com>
2024-10-17 14:13:43 -0700

    Bug Fixes
    Bug Fixes

--- BP/src/Main/commands.ts
+++ BP/src/Main/commands.ts
@@ -2123,7 +2123,7 @@ stack of 16 unbreaking 3 mending 1 shields that are locked to a specific slot an
                     let output = `Preset ${v.slice(12)}: `
                     output+=`Row 0: ${contents.slice(0, 9).filter(v=>!!v).length}/${contents.slice(0, 9).length}`
                     for(let i=1; i<(contents.length/9).floor(); i++){
-                        output+=`\n${" ".repeat(Math.max(indent-(i/9).floor(), 0))}Row ${i}: ${contents.slice(0, 9).filter(v=>!!v).length}/${contents.slice(0, 9).length}`
+                        output+=`\n${" ".repeat(Math.max(indent-(i/9).floor(), 0))}Row ${i}: ${contents.slice(i*9, (i+1)*9).filter(v=>!!v).length}/${contents.slice(i*9, (i+1)*9).length}`
                     }
                     return output
                 }).join("§r\n")
