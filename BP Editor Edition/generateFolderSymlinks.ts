9176417d341abb59da53c4f57480a0e7197e4cc8
8Crafter (PC) <8crafteryt@gmail.com>
2025-10-28 11:31:01 -0700

    v1.41.0-preview.20+BUILD.1

---
+++ BP Editor Edition/generateFolderSymlinks.ts
@@ -0,0 +1,27 @@
+import { existsSync, symlinkSync } from "node:fs";
+import path from "node:path";
+
+const folders: string[] = ["blocks", "dialogue", "entities", "functions", "item_catalog", "items", "loot_tables", "recipes", "scripts", "structures"];
+const files: string[] = ["contents.json"];
+const sourceFolder: string = path.join(process.argv[1]!, "../../BP/");
+const distFolder: string = path.join(process.argv[1]!, "../");
+
+for (const folder of folders) {
+    const sourcePath: string = path.join(sourceFolder, folder);
+    if (!existsSync(sourcePath)) {
+        console.warn(`Folder "${folder}" does not exist in the source folder (${sourceFolder}). Skipping...`);
+        continue;
+    }
+    const distPath: string = path.join(distFolder, folder);
+    if (!existsSync(distPath)) symlinkSync(sourcePath, distPath, "junction");
+}
+
+for (const file of files) {
+    const sourcePath: string = path.join(sourceFolder, file);
+    if (!existsSync(sourcePath)) {
+        console.warn(`File "${file}" does not exist in the source folder (${sourceFolder}). Skipping...`);
+        continue;
+    }
+    const distPath: string = path.join(distFolder, file);
+    if (!existsSync(distPath)) symlinkSync(sourcePath, distPath, "file");
+}
